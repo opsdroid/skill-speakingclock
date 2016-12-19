6d95972e5d87d2159e276a32d01902ec61a00f36
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-12-19 16:09:15 +0000

    Import message class

--- speakingclock.py
+++ speakingclock.py
@@ -1,11 +1,10 @@
-import logging
 from time import gmtime, strftime
 
 from opsdroid.skills import match_crontab
+from opsdroid.message import Message
 
 @match_crontab('* * * * *')
 async def speaking_clock(opsdroid):
-    logging.debug("Running speaking clock")
     connector = opsdroid.default_connector
-    message = Message("placeholder", connector.default_room, None, connector)
+    message = Message("", connector.default_room, None, connector)
     await message.respond(strftime("The time is now %H:%M", gmtime()))
