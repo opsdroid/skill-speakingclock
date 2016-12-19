bd1804d96e496ac1a6e6eb7ce130d60c8968ad7e
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-12-19 15:36:24 +0000

    Speaking clock

--- hello.py
+++ hello.py
@@ -1,16 +1,7 @@
-from opsdroid.skills import match_regex
-import logging
-import random
+from time import gmtime, strftime
 
-def setup(opsdroid):
-    logging.debug("Loaded hello module")
+from opsdroid.skills import match_crontab
 
-@match_regex(r'hi|hello|hey|hallo')
-async def hello(opsdroid, message):
-    text = random.choice(["Hi {}", "Hello {}", "Hey {}"]).format(message.user)
-    await message.respond(text)
-
-@match_regex(r'bye( bye)?|see y(a|ou)|au revoir|gtg|I(\')?m off')
-async def goodbye(opsdroid, message):
-    text = random.choice(["Bye {}", "See you {}", "Au revoir {}"]).format(message.user)
-    await message.respond(text)
+@match_crontab('* * * * *')
+async def speaking_clock(opsdroid):
+    await message.respond(strftime("The time is now %H:%M", gmtime()))
