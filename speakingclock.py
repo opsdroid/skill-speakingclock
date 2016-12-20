4ff3146bb66c0f50480aaa11255b81824ca51534
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-12-20 11:03:50 +0000

    Fix stupid error

--- speakingclock.py
+++ speakingclock.py
@@ -6,5 +6,5 @@ from opsdroid.message import Message
 @match_crontab('* * * * *')
 async def speaking_clock(opsdroid):
     connector = opsdroid.default_connector
-    message = Message("", connector.default_room, None, connector)
+    message = Message("", None, connector.default_room, connector)
     await message.respond(strftime("The time is now %H:%M", gmtime()))
