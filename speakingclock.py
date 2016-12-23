7a6d7940b46bfa518a1787fd2b6721c5c382ba3c
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-12-23 10:02:02 +0000

    Check for message

--- speakingclock.py
+++ speakingclock.py
@@ -5,13 +5,14 @@ from opsdroid.message import Message
 
 @match_crontab('0 * * * *')
 @match_regex(r"what time is it\?")
-async def speaking_clock(opsdroid):
+async def speaking_clock(opsdroid, message):
 
     # Get the default connector
     connector = opsdroid.default_connector
 
-    # Create an empty message to respond to
-    message = Message("", None, connector.default_room, connector)
+    # Create an empty message to respond to if triggered by the crontab
+    if message is None:
+        message = Message("", None, connector.default_room, connector)
 
     # Respond with the current time
     await message.respond(strftime("The time is now %H:%M", gmtime()))
